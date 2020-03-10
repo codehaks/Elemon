c276deea5c7ba331ff9b6711830f62be8a21f26a
Hakim Ghods <ghods.hakim@hotmail.com>
2020-03-11 01:44:06 +0330

    Init

---
+++ src/Portal/Persistance/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Portal.Persistance
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
